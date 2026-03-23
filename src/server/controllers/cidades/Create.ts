955cb9f6711442507c3ebbb74b778029493a5661
Carlos Eduardo <cadu4duarte@gmail.com>
2026-03-23 15:43:31 -0300

    feat: create controllers

---
+++ src/server/controllers/cidades/Create.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express"
+import { StatusCodes } from "http-status-codes"
+import * as yup from "yup"
+
+interface ICidade {
+    nome:string
+}
+
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3),
+})
+
+export const create = async(req: Request<{}, {}, ICidade>, res: Response) => {
+    let validetedData: ICidade | undefined = undefined
+
+    try {
+        validetedData = await bodyValidation.validate(req.body)
+    } catch (error) {
+        const yupError = error as yup.ValidationError
+
+        return res.json({
+            errors: {
+                default: yupError.message,
+            }
+        })
+    }
+
+    console.log(req.body)
+
+    return res.send('create')
+}
