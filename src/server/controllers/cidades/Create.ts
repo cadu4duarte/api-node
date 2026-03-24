4cc6aaf61edf12c2298968cfa44b62b8348ca796
Carlos Eduardo <cadu4duarte@gmail.com>
2026-03-24 14:46:02 -0300

    fix: errors configuration - yup

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -3,29 +3,34 @@ import { StatusCodes } from "http-status-codes"
 import * as yup from "yup"
 
 interface ICidade {
-    nome:string
+    nome:string,
+    estado: string
 }
 
 const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
     nome: yup.string().required().min(3),
+    estado: yup.string().required().min(3),
 })
 
 export const create = async(req: Request<{}, {}, ICidade>, res: Response) => {
     let validetedData: ICidade | undefined = undefined
 
     try {
-        validetedData = await bodyValidation.validate(req.body)
-    } catch (error) {
-        const yupError = error as yup.ValidationError
+        validetedData = await bodyValidation.validate(req.body, {abortEarly: false})
+    } catch (err) {
+        const yupError = err as yup.ValidationError
+        const errors: Record<string, string> = {}
 
-        return res.json({
-            errors: {
-                default: yupError.message,
-            }
+        yupError.inner.forEach(error => {
+            if (error.path === undefined) return
+            errors[error.path] = error.message
+            
         })
+
+        return res.status(StatusCodes.BAD_REQUEST).json({errors})
     }
 
-    console.log(req.body)
+    console.log(validetedData)
 
     return res.send('create')
 }
